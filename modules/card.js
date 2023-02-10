67864afe76b41eafb1e2b45512331ec0307d480b
andja <andja.velimirovic@gmail.com>
2023-02-10 11:18:18 +0100

    add slide indicators to cardImageWrapper

--- modules/card.js
+++ modules/card.js
@@ -4,6 +4,14 @@ import { cardImageWrapper } from "./cardImageWrapper.js";
 export const card = (data) => {
   const card = document.createElement("div");
   card.className = "card";
+
+  const cloneNode = (n, node, parent) => {
+    for (let i = 0; i < n; i++) {
+      const clonedNode = node.cloneNode(true);
+      parent.append(clonedNode);
+    }
+  };
+
   const {
     title,
     description,
@@ -17,7 +25,7 @@ export const card = (data) => {
   } = data;
   //   append nested elements to card & pass needed properties
   card.append(
-    cardImageWrapper(img_url, flag),
+    cardImageWrapper(img_url, flag, cloneNode),
     cardContent(
       title,
       description,
--- modules/cardImageWrapper.js
+++ modules/cardImageWrapper.js
@@ -1,10 +1,23 @@
 import { cardImage } from "./cardImage.js";
 import { imageTopContent } from "./imageTopContent.js";
 
-export const cardImageWrapper = (img_url, flag) => {
+export const cardImageWrapper = (img_url, flag, cloneNode) => {
   const imageWrapper = document.createElement("div");
   imageWrapper.classList = "card-img stacked";
 
-  imageWrapper.append(cardImage(img_url), imageTopContent(flag));
+  const slideIndicators = document.createElement("div");
+  slideIndicators.className = "slide-indicators";
+
+  const indicator = document.createElement("span");
+  indicator.className = "indicator";
+
+  //append indicator node 5 times
+  cloneNode(5, indicator, slideIndicators);
+
+  imageWrapper.append(
+    cardImage(img_url),
+    imageTopContent(flag),
+    slideIndicators
+  );
   return imageWrapper;
 };
