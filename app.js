bc6094a9de1f6bdfb04d542ba2c7df624c7f2aed
andja <andja.velimirovic@gmail.com>
2023-03-02 21:19:12 +0100

    create new App function, early return

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
 import { cardsSection } from "./components/cardsSection/cardsSection.js";
 import { header } from "./components/header/header.js";
-import { PropertyController } from "./handleData.js";
+import { PropertyController } from "./propertyController.js";
 import { notFound } from "./components/notFound/notFound.js";
 
-const properties = new PropertyController();
-
-const listOfAllProperties = await properties
-  .fetchData()
-  .then(() => properties.properties);
-
-const handleSearchEvent = (query) => {
-  displayData(properties.filterData(query));
-};
-
-const displayData = async (data) => {
+const App = async () => {
   try {
-    const gridContainer = document.querySelector(".grid-container");
-    const notFoundPage = document.querySelector(".not-found");
+    const propertyCtx = new PropertyController();
+    const data = await propertyCtx.fetchData();
 
-    if (data.length) {
-      notFoundPage
-        ? notFoundPage.replaceWith(cardsSection({ data }))
-        : gridContainer
-        ? gridContainer.replaceWith(cardsSection({ data }))
-        : document.body.append(
-            header({ handleSearchEvent }),
-            cardsSection({ data })
-          );
-    } else {
-      gridContainer?.replaceWith(notFound());
-    }
+    const handleSearchEvent = (query) => {
+      render(propertyCtx.filterData(query));
+    };
+
+    render(data, handleSearchEvent);
   } catch (error) {
     console.log(error);
   }
 };
 
-window.addEventListener("DOMContentLoaded", displayData(listOfAllProperties));
+const render = (data, handleSearchEvent) => {
+  const gridContainer = document.querySelector(".grid-container");
+  const notFoundPage = document.querySelector(".not-found");
+
+  if (!data.length) {
+    gridContainer?.replaceWith(notFound());
+    return;
+  }
+
+  if (notFoundPage) {
+    notFoundPage.replaceWith(cardsSection({ data }));
+    return;
+  }
+
+  if (gridContainer) {
+    gridContainer.replaceWith(cardsSection({ data }));
+    return;
+  }
+
+  document.body.append(header({ handleSearchEvent }), cardsSection({ data }));
+};
+
+window.addEventListener("DOMContentLoaded", App());
