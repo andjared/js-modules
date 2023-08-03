9938675d277d64462a0acaf544cd5ec5b3a32060
andja <andja.velimirovic@gmail.com>
2023-08-03 14:08:55 +0200

    Add id to json data

--- renderer.js
+++ renderer.js
@@ -10,44 +10,11 @@ class Renderer {
     this.state = Object.assign({}, data);
   }
 
-  createElement(element) {
-    const { localName, childNodes } = element;
-    let attributes;
-    //skip text nodes for attributes
-    if (localName) {
-      const attrs = element.getAttributeNames();
-      attributes = attrs.map((attr) => {
-        return { [`${attr}`]: element.getAttribute(attr) };
-      });
-    }
-
-    //recursively call fn for each child element
-    const children = [...childNodes].map((child) => {
-      if (!child.localName) {
-        return child.textContent;
-      } else {
-        return this.createElement(child);
-      }
-    });
-
-    const node = hyperScript(localName, attributes, children);
-
-    return node;
-  }
-
-  createVDom(data) {
-    const dom = {};
-    for (let item in data) {
-      const element = card({ ...data[item] });
-      dom[item] = this.createElement(element);
-    }
-    return dom;
-  }
+  createVDom(data) {}
 
   render(data) {
     this.setState(data);
-    const dom = this.createVDom(this.state);
-    console.log(dom);
+    console.log(data);
 
     //sacuvaj referencu ka elementu
   }
