a1068a4cc3d64cdf0b54e69e5c622f53ad5a0d39
andja <andja.velimirovic@gmail.com>
2023-08-11 14:29:29 +0200

    handle rerender based on state values

--- renderer.js
+++ renderer.js
@@ -11,10 +11,10 @@ class Renderer {
     this.diff(data);
   }
 
-  render(appendElements, removeElements) {
+  render() {
     const gridContainer = document.querySelector(".grid-container");
     const notFoundPage = document.querySelector(".not-found");
-    const data = Array.from(appendElements.values());
+    const data = Array.from(this.state.values());
 
     if (!this.state.size) {
       gridContainer?.replaceWith(notFound());
@@ -26,16 +26,16 @@ class Renderer {
       return;
     }
 
+    //remove cards that are already rendered but are not in the new state
     Array.from(gridContainer.children).forEach((child) => {
       const id = parseInt(child.id);
-
-      //remove cards that are already rendered but are not in the new state
-      if (!this.state.has(id) || removeElements.has(id)) {
+      if (!this.state.has(id)) {
         gridContainer.removeChild(child);
       }
     });
 
-    appendElements.forEach((item) => {
+    //append child if it doesnt already exists in dom
+    this.state.forEach((item) => {
       if (!document.getElementById(item.id.toString())) {
         gridContainer.append(card({ ...item }));
       }
@@ -43,26 +43,21 @@ class Renderer {
   }
 
   diff(data) {
-    const appendElements = new Map();
-    const removeElements = new Map();
-
+    //add elements from data that are not already in state
     data.forEach((item) => {
       if (!this.state.has(item.id)) {
-        appendElements.set(item.id, item);
         this.state.set(item.id, item);
       }
     });
 
-    //remove elemente koji postoje u state a ne postoje u data
-
+    //remove elements from state that are not in data
     this.state.forEach((item) => {
       if (!data.includes(item)) {
-        removeElements.set(item.id, item);
         this.state.delete(item.id);
       }
     });
 
-    this.render(appendElements, removeElements);
+    this.render();
 
     return this.state;
   }
